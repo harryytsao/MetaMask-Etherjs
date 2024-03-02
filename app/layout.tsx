c349acce73c41e7f4d22e62fff1ca7eb1733ce66
harry <harrytsao12345@gmail.com>
2024-03-02 16:58:37 -0500

    add toast for no MetaMask

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+
 export const metadata: Metadata = {
   title: 'MetaMask Etherjs Connect',
   description: 'MetaMask Etherjs Connect',
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,24 @@
-"use client"
+'use client'
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
-
   const [walletAddress, setWalletAddress] = useState("");
   const [balance, setBalance] = useState<bigint | null>(null);
   const [showInfo, setShowInfo] = useState(false);
 
   async function requestAccount() {
-    console.log('Requesting account...')
-    if (window.ethereum) {
-      console.log('detected');
-
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setWalletAddress(accounts[0]);
-        setShowInfo(true);
-      } catch (error) {
-        console.log("Error connecting...")
-      }
-    } else {
-      console.log('MetaMask not detected');
+    console.log('detected');
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setWalletAddress(accounts[0]);
+      setShowInfo(true);
+    } catch (error) {
+      console.log("Error connecting...")
     }
   }
 
@@ -38,6 +33,15 @@ export default function Home() {
       } catch (error) {
         console.log(error);
       }
+    } else {
+      try {
+        console.log('a')
+        toast.error('MetaMask not detected.', {
+          position: "top-right"
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
@@ -46,11 +50,15 @@ export default function Home() {
       connectWallet();
     }
   }, [walletAddress])
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <button className="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 mt-3 rounded" onClick={connectWallet}>Connect Wallet</button>
-      {showInfo && <h3 className="mt-3">Wallet Address: {walletAddress}</h3>}
-      {showInfo && <h3 className="mt-3">Ethereum Balance: {balance !== null ? balance.toString() + " ETH": 'Loading...'}</h3>}
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <button className="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 mt-3 rounded" onClick={connectWallet}>Connect Wallet</button>
+        {showInfo && <h3 className="mt-3">Wallet Address: {walletAddress}</h3>}
+        {showInfo && <h3 className="mt-3">Ethereum Balance: {balance !== null ? balance.toString() + " ETH" : 'Loading...'}</h3>}
+      </div>
+      <ToastContainer />
+    </>
   );
 }
